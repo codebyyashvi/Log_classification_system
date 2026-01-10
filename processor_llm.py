f53ca5b319cfa9a9a93eacd756b8cb5a4cfe1601
codebyyashvi <yashvikhatri26@gmail.com>
2026-01-10 16:12:01 +0530

    log classification system

---
+++ processor_llm.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+groq = Groq()
+
+def classify_with_llm(log_msg):
+    prompt = f'''Classify the log message into one of these categories:
+    (1) Workflow Error, (2) Deprecation Warning.
+    If you can't figure out a category, return "Unclassified".
+    Only return the category name. No preamble.
+    Log message: {log_msg}'''
+    chat_completion = groq.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ]
+    )
+    return chat_completion.choices[0].message.content
